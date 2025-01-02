2434406e9f03ab5d1a555607e6d7de62398d85f7
prairie2022 <anthonycho0912@gmail.com>
2025-01-02 16:37:49 +0800

    update (neo)vim keybindings

--- CodeMirrorVim for Overleaf-2025-01-02-3.user.js
+++ CodeMirrorVim for Overleaf-2025-01-02-3.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          CodeMirrorVim for Overleaf
 // @namespace     http://tampermonkey.net/
-// @version       2024-12-30
+// @version       2025-01-02-3
 // @description   modified from https://greasyfork.org/en/scripts/461062-overleaf-editor-custom-vim-keybindings-code-mirror-v6
 // @author        prairie2022
 // @match         https://www.overleaf.com/project/*
@@ -17,32 +17,32 @@
         const { CodeMirror, CodeMirrorVim, extensions } = event.detail;
         // escape
         CodeMirrorVim.Vim.map("jk", "<Esc>", "insert");
-        CodeMirrorVim.Vim.map("jj", "<Esc>A<Space><Esc>v?[^ ]<CR>ld/\"\"<CR>A<Space>", "insert");
+        CodeMirrorVim.Vim.map("jj", "<Esc>A <Esc>v?[^ ]<CR>ld/\"\"<CR>A ", "insert");
         CodeMirrorVim.Vim.map("j", "gj", "normal");
         CodeMirrorVim.Vim.map("k", "gk", "normal");
-        CodeMirrorVim.Vim.map("ii", "diTMP-FLAG{}<Esc>hp?TMP-FLAG<CR>fGvFTs", "visual"); // text -> {text}
+        CodeMirrorVim.Vim.map("ii", "diCUR-POS{}<Esc>hp?CUR-POS<CR>cgn", "visual"); // text -> {text}
         CodeMirrorVim.Vim.map("\"", "<Esc>/\\)|\\]|\\}|\\$|\\{\\}|\\[\\]|\\(\\)|$<CR>/\"\"<CR>a", "insert");
         // transform
-        CodeMirrorVim.Vim.map("zL", "?\\$<CR>xv$do\\[<Esc>o<Esc>pA<Space><Esc>0", "normal");
-        CodeMirrorVim.Vim.map("zR", "/\\$<CR>xv$do\\]<Esc>o<Esc>p^", "normal");
-        CodeMirrorVim.Vim.map("zI", "?\\[<CR>dd^v$dkA<Space>$<Esc>pjdd/\\]<CR>kA$<Space><Space><CR><Esc>jddI<Space><Esc>^hv?\\$<CR>lls", "normal");
-        CodeMirrorVim.Vim.map("zA", "?\\[<CR>lsbegin{align*}<Esc>jV/\\]<CR>k>/\\]<CR>lsend{align*}<Esc>", "normal");
-        CodeMirrorVim.Vim.map("zm", ">gvdko\\begin{itemize}<Esc>o\\end{itemize}<Esc>kpI\\item<Space>", "visual"); // itemize, not containing last line
+        CodeMirrorVim.Vim.map("zL", "?\\$\\\\displaystyle |\\$<cr>dgnv$do\\[<Esc>o<Esc>p0", "normal"); // $ -> \[
+        CodeMirrorVim.Vim.map("zR", "/\\$<CR>cgn-<Esc>v$do\\]<Esc>o<Esc>p^x", "normal"); // $ -> \]
+        CodeMirrorVim.Vim.map("zI", "?\\\\\\[<CR>jI$<Esc>kdd/\\\\\\]<CR>kA$<Esc>jdd", "normal"); // \[ \] -> $ $
+        CodeMirrorVim.Vim.map("zA", "?\\\\\\[<CR>cgn\\begin{align*}<Esc>jV/\\\\\\]<CR>k>cgn\\end{align*}<Esc>", "normal"); // \[ \] -> align*
+        CodeMirrorVim.Vim.map("zm", ">gvdko\\begin{itemize}<Esc>o\\end{itemize}<Esc>kpI\\item ", "visual"); // itemize, can't contain last line
         // function
         CodeMirrorVim.Vim.map("UU", "<Esc>uuGA", "insert"); // undo
-        CodeMirrorVim.Vim.map(",d", "TMP-FLAG<Esc>?\\$<CR>a\\displaystyle<Space><Esc>/TMP-FLAG<CR>fGvFTs", "insert"); // displastyle
+        CodeMirrorVim.Vim.map(";d", "CUR-POS<Esc>?\\$\\\\displaystyle |\\$<cr>cgn$\\displaystyle <Esc>/CUR-POS<CR>cgn", "insert"); // displastyle
         // error
         CodeMirrorVim.Vim.map("^^", "^{}<Esc>i", "insert");
         CodeMirrorVim.Vim.map("__", "_{}<Esc>i", "insert");
-        CodeMirrorVim.Vim.map(",v", "^{-1}", "insert");
         CodeMirrorVim.Vim.map("TH", "Th", "insert");
         CodeMirrorVim.Vim.map("taht", "that", "insert");
         // abbreviation
-        CodeMirrorVim.Vim.map(",a", "\\forall\\,<Space>", "insert");
-        CodeMirrorVim.Vim.map(",b", "\\begin{", "insert");
-        CodeMirrorVim.Vim.map(",e", "\\exists\\,<Space>", "insert");
-        CodeMirrorVim.Vim.map(",t", "\\text{}<Esc>i", "insert");
-        CodeMirrorVim.Vim.map(",q", "\"", "insert");
+        CodeMirrorVim.Vim.map(";a", "\\forall\\, ", "insert");
+        CodeMirrorVim.Vim.map(";b", "\\begin{", "insert");
+        CodeMirrorVim.Vim.map(";e", "\\exists\\, ", "insert");
+        CodeMirrorVim.Vim.map(";t", "\\text{}<Esc>i", "insert");
+        CodeMirrorVim.Vim.map(";v", "^{-1}", "insert");
+        CodeMirrorVim.Vim.map(";q", "\"", "insert");
         // User-defined commands
         CodeMirrorVim.Vim.defineEx("home", undefined, buctton_click_ProjectList);
         CodeMirrorVim.Vim.defineEx("fd", undefined, buctton_click_forward_search); // ref: https://discuss.codemirror.net/t/vim-how-to-use-defineex/738/2
---
+++ vscode_neovim/vscode.lua
@@ -0,0 +1,57 @@
+vim.keymap.set('n', '<Esc>', ':noh<cr>', { noremap = true, silent = true })
+
+-- add \displaystyle
+vim.keymap.set('i', '<c-d>', 'CUR-POS<Esc>?\\$\\\\displaystyle \\|\\$<cr>cgn$\\displaystyle<space><Esc>/CUR-POS<cr><Esc>cgn', { silent = true })
+
+-- inline -> display in .tex
+vim.api.nvim_create_user_command('DisplayTeX', function()
+    vim.api.nvim_input('?\\$\\\\displaystyle \\|\\$<cr>cgn')
+    vim.api.nvim_input('<cr>\\[<Esc>/\\$<cr>cgn\\]<cr><Esc><Esc>')
+end, {})
+vim.cmd("cnoreabbrev dt DisplayTeX")
+
+
+-- inline -> display in .md
+vim.api.nvim_create_user_command('DisplayMD', function()
+    vim.api.nvim_input('?\\$\\\\displaystyle \\|\\$<cr>cgn')
+    vim.api.nvim_input('$$<Esc>/\\$<cr>a$<Esc><Esc>')
+end, {})
+vim.cmd("cnoreabbrev dm DisplayMD")
+
+-- synctex
+vim.api.nvim_create_user_command('JumpToPDF', function()
+    vim.fn.VSCodeNotify('latex-workshop.synctex')
+end, {})
+vim.cmd("cnoreabbrev fd JumpToPDF")
+
+-- scroll sync in MD
+vim.api.nvim_create_user_command('ScrollSync', function()
+    vim.fn.VSCodeNotify('markdown-preview-enhanced.toggleScrollSync')
+end, {})
+vim.cmd("cnoreabbrev ss ScrollSync")
+
+-- MD preview
+vim.api.nvim_create_user_command('PreviewMD', function()
+    vim.fn.VSCodeNotify('markdown-preview-enhanced.openPreviewToTheSide')
+end, {})
+vim.cmd("cnoreabbrev pv PreviewMD")
+
+-- MD preview (new tab)
+vim.api.nvim_create_user_command('PreviewInNewTab', function()
+    vim.fn.VSCodeNotify('markdown-preview-enhanced.openPreview')
+end, {})
+vim.cmd("cnoreabbrev PV PreviewInNewTab")
+
+
+-- LaTeX preview
+vim.api.nvim_create_user_command('ViewPDF', function()
+    vim.fn.VSCodeNotify('latex-workshop.view')
+end, {})
+vim.cmd("cnoreabbrev vw ViewPDF")
+
+
+-- primary side bar
+vim.api.nvim_create_user_command('Sidebar', function()
+    vim.fn.VSCodeNotify('workbench.action.toggleSidebarVisibility')
+end, {})
+vim.cmd("cnoreabbrev sd Sidebar")
