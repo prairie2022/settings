d590a99b8ac3578f42d3ad68b4897a119021ea85
prairie2022 <anthonycho0912@gmail.com>
2025-03-04 21:00:37 +0800

    update latex templates

--- sync.sh
+++ sync.sh
@@ -2,10 +2,10 @@
 sudo echo "Start!"
 echo ""
 echo "<course>"
-rclone sync ~/Desktop/course/113-2 drive_50G:course/113-2 --progress --track-renames
+rclone sync ~/Desktop/course/113-2 drive_50G:course/113-2 --progress --track-renames --skip-links
 echo ""
 echo "<data>"
-rclone sync ~/Desktop/data drive_50G:data --progress --track-renames
+rclone sync ~/Desktop/data drive_50G:data --progress --track-renames --skip-links
 echo ""
 echo "<upgrade>"
 sudo dnf upgrade
